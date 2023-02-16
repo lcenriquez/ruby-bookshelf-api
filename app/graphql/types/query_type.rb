4ffd076c07d3151f8445a5ed112e1bcf8ace185a
lcenriquez <luiscarlos_enriquez@me.com>
2023-02-16 11:29:16 -0600

    Initial commit

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,19 @@
+module Types
+  class QueryType < Types::BaseObject
+    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
+
+    # Add root-level fields here.
+    # They will be entry points for queries on your schema.
+
+    # TODO: remove me
+    field :test_field, String, null: false, description: "An example field added by Luis" do
+      argument :name, String, required: true
+    end
+    def test_field(name: String)
+      Rails.logger.info context[:time]
+      "Hello #{name}"
+    end
+  end
+end
---
+++ db/migrate/20230216164407_create_authors.rb
@@ -0,0 +1,13 @@
+class CreateAuthors < ActiveRecord::Migration[7.0]
+  def change
+    create_table :authors do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :string
+      t.integer :yob
+      t.boolean :is_alive
+
+      t.timestamps
+    end
+  end
+end

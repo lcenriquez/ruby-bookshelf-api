86fac3ba533bb8f3896c02de5eddf67c87533358
lcenriquez <luiscarlos_enriquez@me.com>
2023-02-16 21:09:45 -0600

    refactor: extract create_author mutation definition to Mutations::CreateAuthor

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,14 +1,5 @@
 module Types
   class MutationType < Types::BaseObject
-    field :create_author, Types::AuthorType, null: true, description: "Create an author" do
-      argument :first_name, String, required: false
-      argument :last_name, String, required: false
-      argument :yob, Int, required: false
-      argument :is_alive, Boolean, required: false
-    end
-
-    def create_author(first_name:, last_name:, yob:, is_alive:)
-      Author.create(first_name: first_name, last_name: last_name, yob: yob, is_alive: is_alive)
-    end
+    field :create_author, Types::AuthorType, mutation: Mutations::CreateAuthor
   end
 end

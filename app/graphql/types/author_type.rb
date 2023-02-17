1bbd737482a368a7ba494c0f9acb98893c4be931
lcenriquez <luiscarlos_enriquez@me.com>
2023-02-16 21:33:13 -0600

    refactor: extract AuthorInputType
    This allows us to use the author variable from GraphiQL

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -17,3 +17,13 @@ class Types::AuthorType < Types::BaseObject
     "#{object.last_name} #{object.first_name}"
   end
 end
+
+class Types::AuthorInputType < GraphQL::Schema::InputObject
+  graphql_name "AuthorInputType"
+  description "All the attributes needed to create an author"
+
+  argument :first_name, String, required: false
+  argument :last_name, String, required: false
+  argument :yob, Int, required: false
+  argument :is_alive, Boolean, required: false
+end

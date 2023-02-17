86330a385c392f1bc3018caf4d589d90063b6231
lcenriquez <luiscarlos_enriquez@me.com>
2023-02-16 21:49:00 -0600

    feat: update author mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,13 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_author, Types::AuthorType, mutation: Mutations::CreateAuthor
+    field :update_author, Boolean, null: false, description: "Update an author" do
+      argument :author, Types::AuthorInputType, required: true
+    end
+
+    def update_author(author:)
+      existing = Author.where(id: author[:id]).first
+      existing&.update(author.to_h)
+    end
   end
 end

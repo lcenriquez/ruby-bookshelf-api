e130f39eb8632e340f769fbfbab96111dde11b1f
lcenriquez <luiscarlos_enriquez@me.com>
2023-02-21 08:02:37 -0600

    feat: add visibility check to UserType

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,4 +4,11 @@ class Types::UserType < Types::BaseObject
   field :id, ID, null: true
   field :email, String, null: true
   field :is_superadmin, Boolean, null: true
+
+  def self.visible?(context)
+    # This will hide all fields that return UserType in the schema.
+    # For example: when we log out, the CurrentUser query will no longer work.
+    # This is because the current_user query will not be accessible
+    !!context[:current_user]
+  end
 end
